b3b61a41b2315d97791feb762ac9546ef568e0d1
dr-silence <dr.silence@mail.ru>
2022-02-27 17:50:38 +0700

    Реализовано конкурентное чтение данных.

--- hhlfc/hhlfc.cpp
+++ hhlfc/hhlfc.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <iterator>
+#include <optional>
+#include <mutex>
+#include <list>
+
+
+template<class I, class M>
+void ProcessData(I& next, I end, M& mutex)
+{
+	using value_type = typename std::iterator_traits<I>::value_type;
+
+	while(true)
+	{
+		std::optional<value_type> value;
+		{
+			std::lock_guard lock(mutex);
+			if (next != end) value = *next++;
+		}
+
+		if (!value) break;
+	}
+}
 
 int main()
 {
-    return 0;
+	std::list<int> numbers;
+	numbers.push_back(2); numbers.push_back(3);
+
+	auto begin = numbers.begin();
+	std::mutex mutex;
+
+	ProcessData(begin, numbers.end(), mutex);
+
+	return 0;
 }
