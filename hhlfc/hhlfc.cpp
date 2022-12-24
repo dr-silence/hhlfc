a9967c5b035202bcd7598612489feda8c67e91d1
dr-silence <dr.silence@mail.ru>
2022-12-24 23:59:17 +0700

    Значения помещаются в общйи контейнер.

--- hhlfc/hhlfc.cpp
+++ hhlfc/hhlfc.cpp
@@ -7,8 +7,9 @@
 #include <functional>
 #include <thread>
 
-template<class I, class M, class Consumer>
-void ProcessData(I& next, I end, M& mutex, Consumer consumeData)
+// todo: Использовать boost::synchronized_value вместо явного использования мьютекса?
+template<class I, class OI, class M, class Consumer>
+void ProcessData(I& next, I end, OI& out, M& mutex, Consumer consumeData)
 {
 	using value_type = typename std::iterator_traits<I>::value_type;
 
@@ -17,7 +18,11 @@ void ProcessData(I& next, I end, M& mutex, Consumer consumeData)
 		std::optional<value_type> value;
 		{
 			std::lock_guard lock(mutex);
-			if (next != end) value = *next++;
+			if (next != end)
+			{
+				*out++ = *next;
+				value = *next++;
+			}
 		}
 
 		if (!value) break;
@@ -35,6 +40,7 @@ void DelayThisThread()
 #endif
 };
 
+// todo: Реализовать класс представления простого числа: порядковый номер простого числа, значение простого числа.
 using data_type = int;
 
 int main()
@@ -53,17 +59,20 @@ int main()
 
 	std::list<data_type> data1, data2;
 
-	auto consumeData = [](auto& data, auto value) {data.push_back(value); DelayThisThread(); };
+	auto consumeData = [](auto& data, auto value) { data.push_back(value); DelayThisThread(); };
 
 	using consume_data = std::function<void(data_type)>;
 	consume_data consumeData1 = std::bind(consumeData, std::ref(data1), std::placeholders::_1);
 	consume_data consumeData2 = std::bind(consumeData, std::ref(data2), std::placeholders::_1);
 
-	auto processData = std::bind(ProcessData<decltype(numbers)::iterator, decltype(mutex), consume_data>, std::ref(begin), numbers.end(), std::ref(mutex), std::placeholders::_1);
+	std::vector<decltype(numbers)::value_type> result;
+	result.reserve(numbers.size());
+	auto resultInserter = std::back_inserter(result);
+	// todo: const_iterator?
+	auto processData = std::bind(ProcessData<decltype(begin), decltype(resultInserter), decltype(mutex), consume_data>, std::ref(begin), numbers.end(), std::ref(resultInserter), std::ref(mutex), std::placeholders::_1);
 
 	auto processData1 = std::bind(processData, consumeData1);
 	auto processData2 = std::bind(processData, consumeData2);
-
 
 	std::thread thread1(processData1), thread2(processData2);
 
